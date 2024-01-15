12c06a86d3f25a777cbfaf5e2f3b34d69c2b3ede
Juliana Maria <juliana.ramos01@fatec.sp.gov.br>
2024-01-15 16:58:37 -0300

    :lipstick: feat: input style

--- script.js
+++ script.js
@@ -9,23 +9,40 @@ function encurtarUrl() {
         "Content-Type": "application/json",
         "apikey": "Sua API Key",
     }
- 
+
     let linkRequest = {
         destination: document.getElementById("input-url").value,
         domain: { fullName: "rebrand.ly" }
-      }
+    }
  
     fetch("https://api.rebrandly.com/v1/links", {
         method: "POST",
         headers: headers,
         body: JSON.stringify(linkRequest)
-    }).then(response => response.json()).then(json => { 
+    }).then(response => {
+        if (!response.ok) {
+            throw new Error("Erro na requisição. Status: " + response.status);
+        }
+        console.log(response.status)
+        return response.json();
+        
+    })
+    .then(json => {
         let input = document.getElementById("input-url");
         input.value = json.shortUrl;
-     });
+    })
+    .catch(error => {
+        alert("Erro ao encurtar a URL: " + error.message);
+    });
 }
 
 function copiar() {
+    let url = document.getElementById("input-url").value;
+    if(!url){
+        alert("Sem URL para copiar!");
+        return;
+    }
+
     var copyText = document.getElementById("input-url");
    
     copyText.select();
